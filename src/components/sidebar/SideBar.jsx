457ef4caf83f54f1dc86d4f7ae9cde71efa5d1ba
Abnos <abnos5525@gmail.com>
2024-01-01 15:12:43 +0330

    mobile responsive problem fixed

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -3,14 +3,13 @@ import ChipsInfo from "../filtersection/ChipsInfo.jsx";
 
 const SideBar = ({setDrawerOpen,drawerOpen,onGenreChange}) =>{
     return(
-        <Drawer xs={1} sm={0} md={0} lg={0} xl={0} sx={{
+        <Drawer xs={1} sm={0} md={0} lg={0} xl={0} sx={{ float:"left",position:"absolute",
+            left:0,
             height:"100vh",
             overflowX:"hidden",overflowY:"auto", zIndex:"6",
             "& .MuiDrawer-paper": {
                 width: 230,
                 backgroundColor:"primary.main",
-                position:"absolute",
-                left:0
             },
             display:{
                 xs:"block",
