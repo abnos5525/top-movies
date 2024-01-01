fef6fec99abc0b33ffdc6043d33c24c8968a8173
Abnos <abnos5525@gmail.com>
2024-01-01 21:32:30 +0330

    problem testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,19 +8,19 @@ const Header = ({setDrawerOpen}) =>{
         <Box sx={{width:"100%", backgroundColor:"primary.main",textAlign:"center",
             height:"70px",borderBottom:1, borderBottomColor:"#6b6b6b",boxShadow:"2px 5px 3px rgba(0,0,0,.4)"}}>
 
-            <Fab onClick={()=>setDrawerOpen(true)} variant="extended" sx={{
-                position:"absolute", left:20, top:10,
-                display:{
-                    xs:"block",
-                    sm:"none",
-                    md:"none",
-                    lg:"none",
-                    xl:"none"
-                },
-                fontSize:"10pt", fontWeight:"bold", zIndex:2
-            }}>
-                انتخاب ژانر
-            </Fab>
+            {/*<Fab onClick={()=>setDrawerOpen(true)} variant="extended" sx={{*/}
+            {/*    position:"absolute", left:20, top:10,*/}
+            {/*    display:{*/}
+            {/*        xs:"block",*/}
+            {/*        sm:"none",*/}
+            {/*        md:"none",*/}
+            {/*        lg:"none",*/}
+            {/*        xl:"none"*/}
+            {/*    },*/}
+            {/*    fontSize:"10pt", fontWeight:"bold", zIndex:2*/}
+            {/*}}>*/}
+            {/*    انتخاب ژانر*/}
+            {/*</Fab>*/}
 
                 <Typography variant="h4" sx={{textAlign:"center",lineHeight:1.6,cursor:"pointer"}}
                             onClick={()=>navigate("/top-movies")}>
